7d67e478443d180096d0448638bc9a2d6725bb1a
Charles Nii Adotey Allotey <charlesallotey1995@gmail.com>
2025-06-01 20:42:42 +0000

    update

--- components/landing/HeroSection.vue
+++ components/landing/HeroSection.vue
@@ -1,8 +1,8 @@
 <template>
-  <section class="bg-gradient-to-r from-sky-600 to-sky-800 text-white py-20 md:pt-40 md:pb-40">
-    <div class="container flex mx-auto px-4">
+  <section class="bg-gradient-to-r from-sky-600 to-sky-800 text-white py-20 md:pt-20 md:pb-40">
+    <div class="container flex gap-x-20 items-center mx-auto px-4">
       <div class="max-w-3xl mx-auto text-left">
-        <h1 class="text-4xl md:text-7xl leading-[110%] font-bold mb-6">
+        <h1 class="text-4xl md:text-7xl leading-[110%] font-semibold mb-6">
           We help your business <span class="relative z-0"><em class="absolute rounded-full -bottom-1 left-0 w-full h-4 bg-orange-500 -z-0"/><span class="relative">grow</span></span> by focusing on real results such as sales, not empty promises
         </h1>
         <p class="text-xl mb-8">
@@ -17,6 +17,7 @@
           </button>
         </div>
       </div>
+      <img src="/images/hero-img.png" alt="Hero Image" class="w-[500px] xl:scale-125 xl:translate-x-10 shrink-0 h-fit">
 <div class="flex-1 h-full bg-white"/>
     </div>
   </section>
